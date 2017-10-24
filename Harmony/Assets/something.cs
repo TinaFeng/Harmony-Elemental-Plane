782d269c710fe5f87b78d07cfe232e5ce602fe05
TinaFeng <xuanfeng1996@gmail.com>
2017-10-24 13:22:49 -0700

    My scene and some stuff
    when you are done with a scene and save. This should show up. Click
    commit, then sync.

---
+++ Harmony/Assets/something.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class something : MonoBehaviour {
+
+
+    //use something to start with your code. The square is the player, the rectangle is your floor
+    //code something here 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

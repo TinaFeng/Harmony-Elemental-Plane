ee3938688051bec5ff7b5f0e1da719dad69fbd00
WendyWjt <jiangtow@uci.edu>
2017-11-02 09:35:23 -0700

    Wendy's Scene and Folder

---
+++ Harmony/Assets/Scene/Wendy/Script/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D Player;
+	// Use this for initialization
+	void Start ()
+    {
+        Player = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Mathf.Abs(Input.GetAxis("Vertical"));
+        Player.velocity = new Vector2(horizontal, 0);
+        if (vertical >= 1)
+        {
+            Player.velocity = new Vector2(horizontal, 0);
+        }
+        
+	}
+}

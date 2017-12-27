888daa55c4d143ec664ddb8b5d514923dc5d7975
kentjitpatima <kentjitpatima@gmail.com>
2017-12-26 21:14:51 -0800

    Main Menu UI
    Added the Main Menu and enabled the Buttons for the Pause UI

---
+++ Harmony/Assets/Scene/Kent/Scripts/MainMenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour {
+	public string MainMenuScene;
+	public string destinationScene; 
+	public void StartGame(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (destinationScene);
+	}
+	public void QuitGame(){
+		Debug.Log("Game will now Quit");
+		Application.Quit ();
+	}
+	public void LoadMainMenu(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (MainMenuScene);
+	} 
+		
+}
+//UnityEngine.SceneManagement.SceneManager.LoadScene(destinationScene);

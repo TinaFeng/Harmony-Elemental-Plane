1643e5648bb05d4fb7036628fe9b55cda74707ec
Weiyan Zhu <weiyanz@uci.edu>
2018-01-06 23:16:27 -0800

    Modify Bullets
    Change pictures to Tina's work. (I also draw a temporary terrible leaf for grass element, we can change it later) Change the damage of different bullets.

---
+++ Harmony/Assets/Personal/Wayne/Scripts/Item/BulletDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDirection : MonoBehaviour {
+    //caculate the velocity and rotate the sprite
+    public int defaultAngle;
+    private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float angle = Mathf.Atan2(rigid.velocity.y, rigid.velocity.x);
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, defaultAngle + angle*180/3.14159f);
+	}
+}

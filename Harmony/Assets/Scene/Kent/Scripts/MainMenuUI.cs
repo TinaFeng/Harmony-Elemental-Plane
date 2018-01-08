8d62e92407d26eaeab256c4a4de56fae54d850f1
kentjitpatima <kentjitpatima@gmail.com>
2018-01-07 17:47:55 -0800

    Credits and MainMenu UI
    Added working Credits code and Return button for Main menu.

--- Harmony/Assets/Scene/Kent/Scripts/MainMenuUI.cs
+++ Harmony/Assets/Scene/Kent/Scripts/MainMenuUI.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour {
+    public GameObject MainMenuCanvas;
+    public GameObject CreditsCanvas;
 	public string MainMenuScene;
-	public string destinationScene; 
-	public void StartGame(){
+	public string destinationScene;
+
+    private void Start(){
+        MainMenuCanvas.SetActive(true);
+        CreditsCanvas.SetActive(false);
+    }
+    
+    public void StartGame(){
 		UnityEngine.SceneManagement.SceneManager.LoadScene (destinationScene);
 	}
+
+    public void Credits()
+    {
+        MainMenuCanvas.SetActive(false);
+        CreditsCanvas.SetActive(true);
+    }
+    public void Return()
+    {
+        Start();
+    }
 	public void QuitGame(){
 		Debug.Log("Game will now Quit");
 		Application.Quit ();
